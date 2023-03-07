ab91cde2942d761a8132b90d246cb8b0f804d7a5
alafyte <xleb.xlebnyj@mail.ru>
2023-03-07 11:46:35 +0300

    Лабораторная работа №2

---
+++ Lab02/ClientT/ClientT.cpp
@@ -0,0 +1,71 @@
+﻿#define _WINSOCK_DEPRECATED_NO_WARNINGS
+
+#include <iostream>
+#include <clocale>
+#include <ctime>
+
+
+#include "stdafx.h"
+#include "Winsock2.h"                
+#pragma comment(lib, "WS2_32.lib")   
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+
+    SOCKET sS; // дескриптор сокета
+    WSADATA wsaData;
+    try
+    {
+        if (WSAStartup(MAKEWORD(2, 0), &wsaData) != 0)
+            throw SetErrorMsgText("Startup:", WSAGetLastError());
+        if ((sS = socket(AF_INET, SOCK_STREAM, NULL)) == INVALID_SOCKET)
+            throw SetErrorMsgText("socket:", WSAGetLastError());
+
+        SOCKADDR_IN serv; // параметры сокета sS
+        serv.sin_family = AF_INET; // используется IP-адресация
+        serv.sin_port = htons(2000); // порт 2000
+        serv.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+        if ((connect(sS, (sockaddr*)&serv, sizeof(serv))) == SOCKET_ERROR)
+            throw SetErrorMsgText("connect:", WSAGetLastError());
+
+        char ibuf[50] = "server: принято ";
+        int  libuf = 0, lobuf = 0;
+
+        int message_amount = 1000;
+
+        const clock_t start = clock();
+        for (int i = 1; i <= message_amount; i++) 
+        {
+            string obuf = "Hello from Client " + to_string(i);
+
+            if ((lobuf = send(sS, obuf.c_str(), strlen(obuf.c_str()) + 1, NULL)) == SOCKET_ERROR) 
+                throw SetErrorMsgText("send: ", WSAGetLastError());
+
+            if ((libuf = recv(sS, ibuf, sizeof(ibuf), NULL)) == SOCKET_ERROR) 
+                throw SetErrorMsgText("recv: ", WSAGetLastError());
+
+            cout << ibuf << '\n';
+        }
+        const clock_t end = clock();
+        const string obuf = "";
+
+        if ((lobuf = send(sS, obuf.c_str(), strlen(obuf.c_str()) + 1, NULL)) == SOCKET_ERROR) 
+            throw SetErrorMsgText("send: ", WSAGetLastError());
+
+        cout << "Время обмена: " << (static_cast<double>(end - start) / CLK_TCK) << " c\n";
+
+        if (closesocket(sS) == SOCKET_ERROR)
+            throw SetErrorMsgText("closesocket:", WSAGetLastError());
+        if (WSACleanup() == SOCKET_ERROR)
+            throw SetErrorMsgText("Cleanup:", WSAGetLastError());
+    }
+    catch (string errorMsgText)
+    {
+        cout << endl << errorMsgText;
+    }
+
+    system("pause");
+    return 0;
+}
---
+++ Lab02/ServerT/ServerT.cpp
@@ -0,0 +1,105 @@
+﻿#define _WINSOCK_DEPRECATED_NO_WARNINGS
+
+#include <iostream>
+#include <clocale>
+#include <ctime>
+
+
+#include "stdafx.h"
+#include "Winsock2.h"                
+#pragma comment(lib, "WS2_32.lib")   
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+
+    SOCKET cC; // дескриптор сокета
+    WSADATA wsaData;
+    try
+    {
+        if (WSAStartup(MAKEWORD(2, 0), &wsaData) != 0)
+            throw SetErrorMsgText("Startup:", WSAGetLastError());
+        if ((cC = socket(AF_INET, SOCK_STREAM, NULL)) == INVALID_SOCKET)
+            throw SetErrorMsgText("socket:", WSAGetLastError());
+
+        SOCKADDR_IN serv; // параметры сокета sS
+        serv.sin_family = AF_INET; // используется IP-адресация
+        serv.sin_port = htons(2000); // порт 2000
+        serv.sin_addr.s_addr = INADDR_ANY;
+
+        if (bind(cC, (LPSOCKADDR)&serv, sizeof(serv)) == SOCKET_ERROR)
+            throw SetErrorMsgText("bind:", WSAGetLastError());
+
+        if (listen(cC, SOMAXCONN) == SOCKET_ERROR)
+            throw SetErrorMsgText("listen:", WSAGetLastError());
+
+        SOCKET cS; // сокет для обмена данными с клиентом
+        SOCKADDR_IN clnt; // параметры сокета клиента
+        memset(&clnt, 0, sizeof(clnt)); // обнулить память
+        int lclnt = sizeof(clnt); // размер SOCKADDR_IN
+        while (true)
+        {
+            if ((cS = accept(cC, (sockaddr*)&clnt, &lclnt)) == INVALID_SOCKET)
+                throw SetErrorMsgText("accept:", WSAGetLastError());
+
+            cout << "\n---- Новый клиент ----\n\n";
+
+            cout << "IP адрес: " << inet_ntoa(clnt.sin_addr) << endl;
+            cout << "Порт: " << ntohs(clnt.sin_port) << endl << endl;
+
+            clock_t start, end;
+            char obuf[50] = "server: принято ";
+            bool flag = true;
+
+            while (true) 
+            {
+                int lobuf = 0;
+                int libuf = 0;
+                char ibuf[50];
+
+
+                if ((libuf = recv(cS, ibuf, sizeof(ibuf), NULL)) == SOCKET_ERROR) 
+                {
+                    if (WSAGetLastError() == WSAECONNRESET) 
+                    {
+                        end = clock();
+                        flag = true;
+                        break;
+                    }
+                    throw SetErrorMsgText("recv: ", WSAGetLastError());
+                }
+                if (flag) 
+                {
+                    start = clock();
+                    flag = !flag;
+                }
+
+                string obuf = ibuf;
+                if ((lobuf = send(cS, obuf.c_str(), strlen(obuf.c_str()) + 1, NULL)) == SOCKET_ERROR) 
+                    throw SetErrorMsgText("send: ", WSAGetLastError());
+
+                if (strcmp(ibuf, "") == 0) 
+                {
+                    flag = true;
+                    end = clock();
+                    cout << "Время обмена: " << ((double)(end - start) / CLK_TCK) << " c\n\n";
+                    break;
+                }
+                cout << ibuf << endl;
+            }
+            if (closesocket(cS) == SOCKET_ERROR)
+                throw SetErrorMsgText("closesocket:", WSAGetLastError());
+        }
+        if (closesocket(cC) == SOCKET_ERROR)
+            throw SetErrorMsgText("closesocket:", WSAGetLastError());
+        if (WSACleanup() == SOCKET_ERROR)
+            throw SetErrorMsgText("Cleanup:", WSAGetLastError());
+    }
+    catch (string errorMsgText)
+    {
+        cout << endl << errorMsgText;
+    }
+
+    system("pause");
+    return 0;
+}
