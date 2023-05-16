151743874b682c5ed6371884972a18637829a45a
alafyte <xleb.xlebnyj@mail.ru>
2023-05-16 12:15:02 +0300

    Лабораторная работа №7

---
+++ Lab07/ClientMS/ClientMS.cpp
@@ -0,0 +1,40 @@
+﻿#include "stdafx.h"
+
+#define NAME L"\\\\.\\mailslot\\Box"
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	cout << "Mailslot Client" << endl;
+
+	HANDLE hM; // дескриптор почтового ящика 
+	DWORD wb; // длина записанного сообщения
+	char wbuf[] = "Hello from Maislot-client"; // буфер вывода 
+	try
+	{
+		if ((hM = CreateFile(NAME,
+			GENERIC_WRITE, // будем писать в ящик
+			FILE_SHARE_READ, // разрешаем одновременно читать 
+			NULL,
+			OPEN_EXISTING, // ящик уже есть
+			NULL, NULL)) == INVALID_HANDLE_VALUE)
+			throw SetErrorMsgText("CreateFileError", WSAGetLastError());
+
+		for (int i = 0; i < 100; i++)
+			if (!WriteFile(hM,
+				wbuf, // буфер
+				sizeof(wbuf), // размер буфера
+				&wb, // записано
+				NULL))
+				throw SetErrorMsgText("ReadFileError", WSAGetLastError());
+
+		strcpy_s(wbuf,  "STOP");
+		if (!WriteFile(hM, wbuf, sizeof(wbuf), &wb, NULL))
+			throw SetErrorMsgText("ReadFileError", WSAGetLastError());
+		
+	}
+	catch(string error)
+	{ 
+		cout << endl << error << endl;
+	}
+}
---
+++ Lab07/ServerMS/ServerMS.cpp
@@ -0,0 +1,59 @@
+﻿#include "stdafx.h"
+
+#define NAME L"\\\\.\\mailslot\\Box"
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    cout << "Mailslot Server" << endl;
+
+    HANDLE hM; // дескриптор почтового ящика 
+    DWORD rb; // длина почитанного сообщения
+    char rbuf[300]; // буфер ввода
+    DWORD time_waiting = 180000; // 3 минуты
+    bool flag = true;
+    clock_t start, end;
+
+    try
+    {
+        if ((hM = CreateMailslot(NAME,
+            300,
+            MAILSLOT_WAIT_FOREVER, // ждать вечно
+            NULL)) == INVALID_HANDLE_VALUE)
+            throw SetErrorMsgText("CreateMailslotError", WSAGetLastError());
+
+        while (true)
+        {
+
+            if (ReadFile(hM,
+                rbuf, // буфер
+                sizeof(rbuf), // размер буфера
+                &rb, // прочитано
+                NULL))
+            {
+                cout << rbuf << endl;
+                if (flag)
+                {
+                    start = clock();
+                    flag = false;
+                }
+            }
+            else
+                throw SetErrorMsgText("ReadFileError", WSAGetLastError());
+
+            if (strcmp(rbuf, "STOP") == 0)
+            {
+                end = clock();
+                cout << "\nВремя обмена: " << ((double)(end - start) / CLK_TCK) << " секунд" << endl;
+                flag = true;
+            }
+        }
+
+        CloseHandle(hM);
+    }
+    catch (string error)
+    {
+        cout << endl << error << endl;
+    }
+}
+
